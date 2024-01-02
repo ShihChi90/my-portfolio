7e72657bb82606ad291ac763bb4bb76f53a4f6a6
scshao <shao2001@gmail.com>
2024-01-02 15:16:55 +0800

    feature: now should reads correct theme state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,11 @@ import Headerbar from "@/lib/components/Headerbar";
 
 export default function Home() {
   return (
-    <main className="bg-base flex w-full flex-col items-center justify-center gap-3">
+    <main className="flex w-full flex-col items-center justify-center gap-3 bg-neutral">
       <Headerbar />
       <div className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="mt-3 text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.js
-          </code>
-        </p>
+        <h1 className="mt-3 text-6xl font-bold">Welcome to my Portfolio</h1>
+        <p className="mt-3 text-2xl">scroll through this page</p>
       </div>
     </main>
   );
--- src/lib/components/Headerbar.tsx
+++ src/lib/components/Headerbar.tsx
@@ -2,30 +2,34 @@
 
 // Add this line to import the CSS styles
 import { useEffect, useState } from "react";
-import { Button, Navbar, Toggle, Divider } from "react-daisyui";
+import { Button, Navbar, Toggle } from "react-daisyui";
 
 export default function Headerbar() {
   const [theme, setTheme] = useState<string>("everforest");
-  const handleChangeTheme = () => {
-    console.log(theme);
-    setTheme(theme === "everforest" ? "lightforest" : "everforest");
+  const handleChangeTheme = (event: string) => {
+    // console.log(event);
+    setTheme(event === "everforest" ? "lightforest" : "everforest");
   };
   useEffect(() => {
     document.querySelector("html")?.setAttribute("data-theme", theme);
   }, [theme]);
 
   return (
-    <Navbar className="justify-between gap-4 bg-secondary">
-      <Button color="primary" className="glass mx-2" size="md">
+    <Navbar className="justify-between bg-secondary">
+      <Button className="glass mx-2 bg-secondary" size="md">
         <a href="/" className="text- text-2xl font-bold">
           Shih-Chi Shao
         </a>
       </Button>
+      {/* <Divider horizontal /> */}
 
-      <div className="gap-2">
-        <Divider horizontal />
+      <div className="gap-2 px-2">
         <p>dark</p>
-        <Toggle className="mx-2" onChange={() => handleChangeTheme()} />
+        <Toggle
+          className="mx-2"
+          value={theme}
+          onChange={(event) => handleChangeTheme(event.target.value)}
+        />
         <p>light</p>
       </div>
     </Navbar>
