f32ff425f689c6e51a1907ecd0f4fdc855987d07
scshao <shao2001@gmail.com>
2024-01-04 15:14:55 +0800

    setup image index

---
+++ src/lib/images/index.ts
@@ -0,0 +1,8 @@
+import nanhu1 from "./nanhu/20220802-_MG_0916.jpg";
+import nanhu2 from "./nanhu/20220803-_MG_1015.jpg";
+import nanhu3 from "./nanhu/20220803-_MG_1024.jpg";
+
+// import more images if needed
+
+const images = [nanhu1, nanhu2, nanhu3];
+export default images;
